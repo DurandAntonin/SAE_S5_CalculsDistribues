c9cd54428bdd2e3747c20c800825029c1b5432cf
Madianou <102082724+Madianou@users.noreply.github.com>
2023-11-22 22:17:51 +0100

    Correction Bug Animation en scroll qui ne s'affichait pas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,php}"],
+  content: {
+  relative: true,
+  transform: (content) => content.replace(/taos:/g, ''),
+  files:["./src/**/*.{html,js,php}"]
+  },
   theme: {
     extend: {
       colors: {
