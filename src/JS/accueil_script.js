43565477f16bdc3a86fa5a72eb8bae0cce424ecc
Madianou <102082724+Madianou@users.noreply.github.com>
2023-11-27 12:29:21 +0100

    Pop up pour modifier les informations de son compte

--- src/JS/accueil_script.js
+++ src/JS/accueil_script.js
@@ -1,6 +1,10 @@
 const profil = document.querySelector('#popUpProfil');
 const showProfile = document.querySelector('#showProfil');
+const formProfil = document.querySelector('#popUpFormProfil');
+const linkShowProfil = document.querySelector('#linkShowProfil');
 var showed = false;
+var showedF = false;
+
 
 function showProfil() {
   console.log("click");
@@ -17,6 +21,24 @@ function showProfil() {
   }
 }
 
+function showFormProfile() {
+  console.log("click2");
+  if (!showedF) {
+    formProfil.classList.remove("hidden");
+    formProfil.classList.add("flex");
+    showedF = true;
+    showProfil();
+    document.addEventListener('click', handleClickOutsideF);
+
+  } else {
+    formProfil.classList.add("hidden");
+    formProfil.classList.remove("flex");
+    showedF = false;
+    document.removeEventListener('click', handleClickOutsideF);
+
+  }
+}
+
 function handleClickOutside(event) {
 
   if (!profil.contains(event.target)) {
@@ -27,7 +49,21 @@ function handleClickOutside(event) {
   }
 }
 
+function handleClickOutsideF(event) {
+
+  if (!formProfil.contains(event.target)) {
+    formProfil.classList.add("hidden");
+    showedF = false;
+
+    document.removeEventListener('click', handleClickOutsideF);
+  }
+}
+
 
 showProfile.addEventListener('click', function(event) {
+  event.stopPropagation();
+});
+
+linkShowProfil.addEventListener('click', function(event) {
   event.stopPropagation();
 });
--- src/PHP/page_accueil_user.php
+++ src/PHP/page_accueil_user.php
@@ -15,7 +15,9 @@ $user = unserialize($_SESSION["user"]);
     <link href="../dist/output.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">  
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>  
 </head>
 <body class="bg-lightblue" style="font-family: 'Poppins', sans-serif;">
 
@@ -30,7 +32,7 @@ $user = unserialize($_SESSION["user"]);
                     <?php
                 //seul le user inscrit peut voir son profil
                 if ($user->getRole() == PHP\Enum_role_user::USER){
-                    echo "<img src='../PICTURES/IconeProfil.png' alt='profile picture' class='h-10 mr-10' onclick='showProfil()' id='showProfil'>   
+                    echo "<img src='../PICTURES/IconeProfil.png' alt='profile picture' class='h-10 mr-10 cursor-pointer' onclick='showProfil()' id='showProfil'>   
                     ";
                 }
                 ?>
@@ -40,7 +42,7 @@ $user = unserialize($_SESSION["user"]);
     <?php
         if ($user->getRole() == PHP\Enum_role_user::USER){
             echo "<div class='hidden top-0 float-right w-60 px-5 py-3 mr-2 dark:bg-gray-800 bg-deepblue rounded-lg shadow border dark:border-transparent animate-fade-down animate-duration-[400ms] animate-ease-in-out' id='popUpProfil'>
-            <ul class='space-y-3 dark:text-white'>
+            <ul class='space-y-3 text-white'>
               <li class='font-medium'>
                 <a href='#' class='flex items-center transform transition-colors duration-200 border-r-4 border-transparent hover:border-indigo-700'>
                   <div class='mr-3'>
@@ -50,7 +52,7 @@ $user = unserialize($_SESSION["user"]);
                 echo "</a>
               </li>
               <li class='font-medium'>
-                <a href='#' class='flex items-center transform transition-colors duration-200 border-r-4 border-transparent hover:border-indigo-700'>
+                <a href='#' class='flex items-center transform transition-colors duration-200 border-r-4 border-transparent hover:border-indigo-700' onclick='showFormProfile()' id='linkShowProfil'>
                   <div class='mr-3'>
                     <svg class='w-6 h-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z'></path><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M15 12a3 3 0 11-6 0 3 3 0 016 0z'></path></svg>
                   </div>
@@ -71,6 +73,50 @@ $user = unserialize($_SESSION["user"]);
                     ";
                 }
                 ?>
+
+<div class="h-screen items-center justify-center hidden" id="popUpFormProfil">
+    <div class="container mx-auto">
+    <div class="my-12 flex items-center justify-center px-6">
+      <!-- Row -->
+     
+        <div class="w-full md:w-1/3 bg-deepblue rounded-3xl items-center relative">
+          <h2 class="text-3xl text-center text-white my-8">Mon profil </h2>
+          <ion-icon name="close" class="text-white right-0 my-9 mr-6 top-0 absolute text-3xl cursor-pointer" onclick="showFormProfile()"></ion-icon>
+          <form class="mb-4 rounded  px-8 pb-8 pt-6" method="post" action="traitement_inscription.php">
+            <div class="mb-4">
+                <label class="mb-2 block text-sm font-bold text-white" for="login"> Identifiant </label>
+                <input class="focus:shadow-outline mb-3 w-full appearance-none rounded border px-3 py-2 text-sm leading-tight text-gray-700 shadow focus:outline-none" id="login" name="login" placeholder="Identifiant" required/>
+            </div>
+            <div class="mb-4 md:flex md:justify-between">
+            
+              <div class="mb-4 md:mb-0 md:mr-2">
+                <label class="mb-2 block text-sm font-bold text-white" for="first_name"> Prénom </label>
+                <input class="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 text-sm leading-tight text-gray-700 shadow focus:outline-none" id="first_name" id="first_name" type="text" placeholder="Prénom" required/>
+              </div>
+              <div class="md:ml-2">
+                <label class="mb-2 block text-sm font-bold text-white" for="last_name"> Nom </label>
+                <input class="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 text-sm leading-tight text-gray-700 shadow focus:outline-none" id="last_name" name="last_name" type="text" placeholder="Nom" required/>
+              </div>
+            </div>
+            <div class="mb-4">
+              <label class="mb-2 block text-sm font-bold text-white" for="email"> Email </label>
+              <input class="focus:shadow-outline mb-3 w-full appearance-none rounded border px-3 py-2 text-sm leading-tight text-gray-700 shadow focus:outline-none" id="email" type="email" name="email" placeholder="Email" required/>
+            </div>
+            <div class="mb-4 md:flex md:justify-between">
+              <div class="mb-4 md:mb-0 md:mr-2">
+                <label class="mb-2 block text-sm font-bold text-white" for="password"> Mot de passe </label>
+                <input class="focus:shadow-outline mb-3 w-full appearance-none rounded border border-red-500 px-3 py-2 text-sm leading-tight text-gray-700 shadow focus:outline-none" id="password" name="password" type="password" placeholder="******************" required/>
+                <p class="text-xs italic text-red-500">Choisissez un mot de passe différent du précédent.</p>
+              </div>
+              <div class="md:ml-2">
+                <label class="mb-2 block text-sm font-bold text-white" for="password_confirm"> Confirmez le mot de passe</label>
+                <input class="focus:shadow-outline mb-3 w-full appearance-none rounded border px-3 py-2 text-sm leading-tight text-gray-700 shadow focus:outline-none" id="password_confirm" name="password_confirm" type="password" placeholder="******************" required/>
+              </div>
+            </div>
+            <div class="mb-2 text-center">
+              <input type="submit" name="submit_inscription" value="Valider" class="w-3/4 mt-6 py-2 rounded-xl bg-lgrey text-white focus:outline-none hover:bg-lyellow hover:text-deepblue focus:ring-4 focus:ring-gray-300 cursor-pointer">
+            </div>
+          </div>
 
 </body>
 </html>
