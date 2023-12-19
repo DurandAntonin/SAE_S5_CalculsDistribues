15bc1ad10557de6f51a51d740793fd5d0ebb7f57
Madianou <102082724+Madianou@users.noreply.github.com>
2023-12-19 13:40:16 +0100

    pop up liste des users - page accueil admin

--- src/JS/accueil_admin_script.js
+++ src/JS/accueil_admin_script.js
@@ -20,3 +20,37 @@ const configPie = {
 };
 
 var chartBar = new Chart(document.getElementById("chartPie"), configPie);
+
+const listUsers = document.querySelector('#popUpUsers');
+const btnShowUsers = document.querySelector('#showUsers');
+
+var showed = false;
+
+function showUsers() {
+    console.log("click");
+    if (!showed) {
+      listUsers.classList.remove("hidden");
+      showed = true;
+  
+      document.addEventListener('click', handleClickOutside);
+    } else {
+      listUsers.classList.add("hidden");
+      showed = false;
+  
+      document.removeEventListener('click', handleClickOutside);
+    }
+  }
+
+function handleClickOutside(event) {
+
+if (!profil.contains(event.target)) {
+    listUsers.classList.add("hidden");
+    showed = false;
+
+    document.removeEventListener('click', handleClickOutside);
+}
+}
+
+btnShowUsers.addEventListener('click', function(event) {
+    event.stopPropagation();
+  });
