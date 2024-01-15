7f426196d8186c65650009cb7dbd93404cdd930a
Antonin Durand <antonin.durand576@gmail.com>
2024-01-15 14:46:30 +0100

    Refonte du système d'appel au script python dans les modules

--- src/PHP/script_calcul_module.php
+++ src/PHP/script_calcul_module.php
@@ -7,6 +7,7 @@ include_once "MySQLDataManagement.php";
 include_once "Enum_niveau_logger.php";
 include_once "Logger.php";
 include_once "LoggerInstance.php";
+include_once "CommandBuilder.php";
 
 session_start();
 
@@ -36,13 +37,12 @@ if (isset($header["Content-Type"]) && $header["Content-Type"] == "application/js
     $listeResultParams = ["error"=>0, "errorMessage"=>"", "result"=>null];
 
     //on regarde quel est le mode utilisé
-    //0 = exécute le script python pour calculer les nombres premiers
+    //0 = exécute un script python
     //1 = on regarde dans le fichier d'indicateur si le programme s'est terminé
     //2 = récupère le résultat du script python (liste des nombres premiers et temps d'exécution)
     if ($mode == 0){
-        $borneMin = $paramExecRequete["bornes"][0];
-        $borneMax = $paramExecRequete["bornes"][1];
         $execMode = $paramExecRequete["execMode"]; // mode d'exécution du script distribué ou non
+        $numModule = $paramExecRequete["numModule"]; //module du site, pour déterminer quel script python exécuter
 
         $outputFileName = guidv4() . ".json"; //fichier qui stocke le résultat du script python
 
@@ -57,48 +57,82 @@ if (isset($header["Content-Type"]) && $header["Content-Type"] == "application/js
         //on attend 1s
         sleep(1);
 
-        $fp = fopen($VARIABLES_GLOBALES["repertoire_resultat"] . $fileNameForHostname, "r");
-        $hostname = trim(fread($fp, 10));
-        //echo $hostname;
-        fclose($fp);
-
-        //la commande change en fonction du role du user et du mode d'exécution (distribué ou non du programme)
-        $command = null;
-        $outputFile = $VARIABLES_GLOBALES["chemin_result_dans_pi"] . $outputFileName;
-        if ($userRole == Enum_role_user::USER && $execMode){
-            //on stocke dans une chaine de caratères la liste des hostname de chaque pi
-            $listHostnames = array();
-            if ($hostname == "pi1")
-                $listHostnames = [$hostname, "pi2", "pi3", "pi4"];
-            elseif ($hostname == "pi2")
-                $listHostnames = [$hostname, "pi3", "pi4"];
-            elseif ($hostname == "pi3")
-                $listHostnames = [$hostname, "pi2", "pi4"];
-            else
-                $listHostnames = [$hostname, "pi2", "pi3"];
-
-            $hostnames = implode(",", $listHostnames);
-            $numberOfNodes = count($listHostnames);
-
-            $command = "echo \"ssh cnat mpiexec -n {$numberOfNodes} --host $hostnames python /home/pi/prime.py {$borneMin} {$borneMax} {$outputFile} --mca btl_tcp_if_include 172.19.181.0/24\" > {$VARIABLES_GLOBALES["chemin_pipe_module_nb_premiers_dans_conteneur"]}";
+        $hostname = "";
+        if (file_exists($VARIABLES_GLOBALES["repertoire_resultat"] . $fileNameForHostname)){
+            $fp = fopen($VARIABLES_GLOBALES["repertoire_resultat"] . $fileNameForHostname, "r");
+            $hostname = trim(fread($fp, 10));
+            //echo $hostname;
+            fclose($fp);
         }
-        elseif (!$execMode && ($userRole == Enum_role_user::USER || $userRole == Enum_role_user::VISITEUR))
-            $command = "echo \"ssh cnat mpiexec -n 1 --host $hostname python /home/pi/prime.py {$borneMin} {$borneMax} {$outputFile} --mca btl_tcp_if_include 172.19.181.0/24\" > {$VARIABLES_GLOBALES["chemin_pipe_module_nb_premiers_dans_conteneur"]}";
 
-        else
-            $command = "";
+        //fichier qui va contenir le résultat de la commande
+        $outputFile = $VARIABLES_GLOBALES["chemin_result_dans_pi"] . $outputFileName;
 
+        //on regarde quel script exécuter en fonction du module
+        //on insère dans une liste les paramètres du script
+        //on regarde dans quel pipe envoyer le script
+        $pathToScript = "";
+        $listScriptParameter = array();
+        $pipeToExecuteScript = "";
+        if ($numModule == 1){
+            $pathToScript = $VARIABLES_GLOBALES["chemin_script_calcul_nombres_premiers"];
+
+            $borneMin = $paramExecRequete["bornes"][0];
+            $borneMax = $paramExecRequete["bornes"][1];
+            $listScriptParameter = [$borneMin, $borneMax, $outputFile];
+
+            $pipeToExecuteScript = $VARIABLES_GLOBALES["chemin_pipe_module_nb_premiers_dans_conteneur"];
+        }
+
+        else if ($numModule == 2){
+            $pathToScript = $VARIABLES_GLOBALES["chemin_script_calcul_pi"];
+
+            $nbLancers = $paramExecRequete["nbLancers"];
+            $listScriptParameter = [$nbLancers, $outputFile];
+
+            $pipeToExecuteScript = $VARIABLES_GLOBALES["chemin_pipe_module_calcul_pi_dans_conteneur"];
+        }
+
+        //on créé une liste pour stocker les hostnames de chaque rpi pour le calcul
+        $listHostnames = array();
+        $listHostnameAvailable = $VARIABLES_GLOBALES["list-hostnames-valides"];
+        if ($userRole == Enum_role_user::USER && $execMode){
+            if ($hostname == $listHostnameAvailable[0])
+                $listHostnames = [$hostname, "pi2", "pi3", "pi4"];
+            elseif ($hostname == $listHostnameAvailable[1])
+                $listHostnames = [$hostname, "pi3", "pi4"];
+            elseif ($hostname == $listHostnameAvailable[2])
+                $listHostnames = [$hostname, "pi2", "pi4"];
+            elseif ($hostname == $listHostnameAvailable[3])
+                $listHostnames = [$hostname, "pi2", "pi3"];
+        }
+        elseif (!$execMode && ($userRole == Enum_role_user::USER || $userRole == Enum_role_user::VISITEUR)){
+            //on vérifie que le hostname existe bien
+            if (in_array($hostname, ["pi1", "pi2", "pi3", "pi4"])){
+                $listHostnames[] = $hostname;
+            }
+        }
+
+        //on créé une liste pour stocker les paramètres de la commande
+        $listCommandParameter = ["--mca ", "btl_tcp_if_include 172.19.181.0/24"];
+
+        //on on construit et build la commande
+        $commandBuilder = new CommandBuilder($pathToScript, $listScriptParameter, $listCommandParameter, $listHostnames, $pipeToExecuteScript);
+        $command = $commandBuilder->buildCommand();
+        //echo $command;
 
         //on regarde s'il y a un problème
         if ($command == ""){
-            //le user n'est pas connecté et essaie d'exécuter le programme de manière distribué, on enregistre l'erreur
+            //le module est inconnu, on enregistre l'erreur
             //on enregistre a l'aide du logger le warning
-            $loggerFile->warning($userId, getTodayDate(), $_SERVER['REMOTE_ADDR'], "Le user n'est pas connecté et essaie d'exécuter le programme de manière distribué");
-            $listeResultParams["error"] = $VARIABLES_GLOBALES["notif_erreur_interne"];
+            $commandStr = $commandBuilder->__toString();
+            $loggerFile->warning($userId, getTodayDate(), $_SERVER['REMOTE_ADDR'], "Erreur lors du build de la Commande : {$commandStr}");
+            $listeResultParams["error"] = 1;
+            $listeResultParams["errorMessage"] = $VARIABLES_GLOBALES["notif_erreur_interne"];
         }
         else{
             //on enregistre à l'aide d'un logger l'utilisation du module par le user
-            $loggerBd->info($userId, getTodayDate(), $_SERVER['REMOTE_ADDR'], "Utilisation module1");
+            $loggerBd->info($userId, getTodayDate(), $_SERVER['REMOTE_ADDR'], "Utilisation module{$numModule}");
 
             //on exécute la commande
             exec($command,$output,$resultCode);
@@ -139,14 +173,16 @@ if (isset($header["Content-Type"]) && $header["Content-Type"] == "application/js
         else{
             //on enregistre a l'aide du logger le warning
             $loggerFile->warning($userId, getTodayDate(), $_SERVER['REMOTE_ADDR'], "Fichier {$outputFile} non présent");
-            $listeResultParams["error"] = $VARIABLES_GLOBALES["notif_erreur_interne"];
+            $listeResultParams["error"] = 1;
+            $listeResultParams["errorMessage"] = $VARIABLES_GLOBALES["notif_erreur_interne"];
         }
     }
 
     else{
         //on enregistre a l'aide du logger le warning
         $loggerFile->warning($userId, getTodayDate(), $_SERVER['REMOTE_ADDR'], "Mode {$mode} dans script_get_stats_cluster_hat inconnu");
-        $listeResultParams["error"] = $VARIABLES_GLOBALES["notif_erreur_interne"];
+        $listeResultParams["error"] = 1;
+        $listeResultParams["errorMessage"] = $VARIABLES_GLOBALES["notif_erreur_interne"];
     }
 
     //en renvoie le résultat des requetes au script js sous format json
